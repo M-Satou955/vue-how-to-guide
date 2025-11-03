99639b1f9cf3f74a05c233fb2b8b3cd3a5a87d74
M-Satou955 <135571499+M-Satou955@users.noreply.github.com>
2025-11-03 20:29:58 +0900

    メモ追加&カスタムブロックの挙動試した

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,30 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from "vue";
+
+// リアクティブな状態
+const count = ref(0);
+
+// 状態を変更し、更新をトリガーする関数。
+function increment() {
+  count.value++;
+}
+
+// ライフサイクルフック
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`);
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<memo lang="md"></memo>
